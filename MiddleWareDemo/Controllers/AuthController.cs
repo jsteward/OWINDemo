f88d54d4891beff2e1951808092d1805385a6609
jsteward <jacques.steward@gmail.com>
2016-03-31 08:23:04 -0500

    Added facebook Login

---
+++ MiddleWareDemo/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using MiddleWareDemo.Models;
+
+namespace MiddleWareDemo.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            var model = new LoginModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (model.Username.Equals("Jacques", StringComparison.OrdinalIgnoreCase)&&model.Password == "password")
+            {
+                var identity = new ClaimsIdentity("ApplicationCookie");
+                identity.AddClaims(new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, model.Username),
+                    new Claim(ClaimTypes.Name, model.Username)
+                });
+                HttpContext.GetOwinContext().Authentication.SignIn(identity);
+            }
+            return View(model);
+        }
+
+        public ActionResult LoginFacebook()
+        {
+            HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties
+            {
+                RedirectUri = "/secret"
+            }, "Facebook");
+            return new HttpUnauthorizedResult();
+        }
+    }
+}
